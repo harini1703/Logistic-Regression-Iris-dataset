a5da3aaa8894838fde4095c44c5ce17bac4b3726
Hari Lakshmi A.S <93150869+harini1703@users.noreply.github.com>
2022-09-16 10:45:16 +0530

    Create Interface.py

---
+++ Interface.py
@@ -0,0 +1,12 @@
+import pickle
+pickle_in=open('flowers.pkl','rb')
+clf=load.(pickle_in)
+
+a=st.number_input('Enter the Sepal Length' )
+b=st.number_input('Enter the Sepal Width'  )
+c=st.number_input('Enter the Petal length' )
+d=st.number_input('Enter the Petal width' )
+
+result=''
+if st.button('Predict'):
+   
